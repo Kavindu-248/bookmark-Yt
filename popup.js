9aad2feddb094be006cfccca08a48fa1b36eaf71
Kavindu Fernando <kavindu2331@gmail.com>
2023-05-22 13:39:38 +0400

    feat: added the play and delete function.

--- popup.js
+++ popup.js
@@ -4,7 +4,27 @@ import { getActiveTabURL } from "./utils.js";
 
 // adding a new bookmark row to the popup
 
-const addNewBookmark = () => {};
+const addNewBookmark = (bookmarksElement, bookmark) => {
+    const bookmarkTitleElement = document.createElement("div");
+    const newBookmarkElement = document.createElement("div");
+    const controlsElement = document.createElement("div");
+
+    bookmarkTitleElement.textContent = bookmark.desc;
+    bookmarkTitleElement.className = "bookmark-title";
+
+    controlsElement.className = "bookmark-controls";
+
+    newBookmarkElement.id = "bookmark-" + bookmark.time;
+    newBookmarkElement.className = "bookmark";
+    newBookmarkElement.setAttribute("timestamp", bookmark.time);
+
+    setBookmarkAttributes("play", onPlay, controlsElement);
+
+    newBookmarkElement.appendChild(bookmarkTitleElement);
+    newBookmarkElement.appendChild(controlsElement);
+    bookmarksElement.appendChild(newBookmarkElement);
+
+};
 
 const viewBookmarks = (currentBookmarks=[]) => {
     const bookmarksElement = document.getElementById("bookmarks");
@@ -13,18 +33,49 @@ const viewBookmarks = (currentBookmarks=[]) => {
     if(currentBookamrks.length > 0){
         for(let i = 0; i < currentBookmarks.length; i++){
             const bookmark = currentBookmarks[i];
-            addNewBookmark(bookmark, bookmarksElement)
+            addNewBookmark(bookmark, bookmarksElement);
         }
     }else{
         bookmarksElement.innerHTML = '<i class="row">No bookmarks yet</i>';
     }
 };
 
-const onPlay = e => {};
+const onPlay = async e => {
+    const bookmarkTime = e.target.parentNode.getAttribute("timestamp");
+    const activeTab = await getActiveTabURL();
 
-const onDelete = e => {};
+    chrome.tabs.sendMessage(activeTab.id, {
+        type: "PLAY",
+        value: bookmarkTime
+    });
 
-const setBookmarkAttributes =  () => {};
+
+};
+
+const onDelete = async e => {
+    const activeTab = await getActiveTabURL();
+    const bookmarkTime = e.target.parentNode.getAttribute("timestamp");
+    const bookmarkElementToDelete = document.getElementById("bookmark-" + bookmarkTime);
+
+    bookmarkElementToDelete.parentNode.removeChild(bookmarkElementToDelete);
+
+    chrome.tabs.sendMessage(activeTab.id, {
+        type: "DELETE",
+        value: bookmarkTime
+
+    }, viewBookmarks);
+
+
+};
+
+const setBookmarkAttributes =  (src, eventListener, controlParentElement) => {
+    const controlElement = document.createElement("img");
+
+    controlElement.src = "assets/" + src + ".png";
+    controlElement.title = src;
+    controlElement.addEventListener("click", eventListener);
+    controlParentElement.appendChild(controlElement);
+};
 
 document.addEventListener("DOMContentLoaded", async () => {
     const activeTab = await getActiveTabURL();
